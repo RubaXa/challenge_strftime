3f298cd1bde255e73d71bb216548ee81344dfa25
RubaXa <ibnRubaXa@gmail.com>
2014-06-30 21:57:00 +0400

    + true benchmark

--- README.md
+++ README.md
@@ -1,8 +1,142 @@
-challenge_strftime
-==================
+# challenge_strftime
 
-The challenge: make strftime 50x faster
+ 1. `npm install`
+ 2. `npm start` or `npm start --filter=date|time|random|arg`
 
-[See results][1]
 
-[1]: https://docs.google.com/spreadsheets/d/1PJCrAz23YDS-n8iinJjqmhCI86Xz89oPuEP-bkEUrQc/edit?usp=sharing
+---
+
+
+## Absolute Winners (timestamp + random)
+```
+#1. siranthony        (7.451 + 12.018)/2 = 9.735
+#2. newbiecraft       (10.010 + 8.633)/2 = 9.322
+#3. mapostol          (10.005 + 8.256)/2 = 9.131
+#4. lexich121         (11.864 + 3.225)/2 = 7.545
+#5. nikitin.alexandr  (10.173 + 4.295)/2 = 7.234
+#6. dm.ashurov        (10.257 + 4.020)/2 = 7.139
+```
+
+
+---
+
+
+## Winners: timestamp
+```
+npm start --filter=time
+-----------------------------------------------------------
+#1. lexich121                 x11.864, wins: 2
+#2. dm.ashurov                x10.257, wins: 0
+#3. nikitin.alexandr          x10.173, wins: 1
+#4. newbiecraft               x10.010, wins: 0
+#5. mapostol                  x10.005, wins: 0
+#6. azazel                    x10.003, wins: 0
+#7. lordblackfox              x8.540, wins: 0
+#8. siranthony                x7.451, wins: 0
+#9. juliasoft                 x6.750, wins: 0
+#10. smitterz                 x6.063, wins: 0
+#11. ibnrubaxa                x5.383, wins: 0
+#12. kuchumovn                x5.344, wins: 0
+#13. sergii.shpak             x5.317, wins: 0
+#14. sairi.na.tenshi          x5.133, wins: 0
+#15. an12345                  x4.870, wins: 0
+#16. rzcoder                  x3.704, wins: 0
+#17. autarc                   x1.636, wins: 0
+#18. karol_wegrzycki          x1.543, wins: 0
+#19. original                 x1.000, wins: 0
+#20. trialforlife             x0.910, wins: 0
+#21. original2                x0.806, wins: 0
+```
+
+
+---
+
+
+## Winners: random
+```
+npm start --filter=random
+-----------------------------------------------------------
+#1. siranthony                x12.018, wins: 3
+#2. newbiecraft               x8.633, wins: 0
+#3. mapostol                  x8.256, wins: 0
+#4. ibnrubaxa                 x7.434, wins: 0
+#5. nikitin.alexandr          x4.295, wins: 0
+#6. smitterz                  x4.057, wins: 0
+#7. dm.ashurov                x4.020, wins: 0
+#8. lordblackfox              x3.481, wins: 0
+#9. lexich121                 x3.225, wins: 0
+#10. azazel                   x3.083, wins: 0
+#11. rzcoder                  x2.956, wins: 0
+#12. juliasoft                x1.822, wins: 0
+#13. an12345                  x1.705, wins: 0
+#14. kuchumovn                x1.129, wins: 0
+#15. original                 x1.000, wins: 0
+#16. sergii.shpak             x0.858, wins: 0
+#17. autarc                   x0.787, wins: 0
+#18. original2                x0.785, wins: 0
+#19. sairi.na.tenshi          x0.784, wins: 0
+#20. karol_wegrzycki          x0.539, wins: 0
+#21. trialforlife             x0.404, wins: 0
+```
+
+
+---
+
+
+## Winners: date (equivocal benchmark)
+```
+npm start --filter=date
+-----------------------------------------------------------
+#1. original2                 x154.446, wins: 3
+#2. sergii.shpak              x82.208, wins: 0
+#3. siranthony                x16.765, wins: 0
+#4. lexich121                 x12.763, wins: 0
+#5. dm.ashurov                x11.756, wins: 0
+#6. newbiecraft               x9.943, wins: 0
+#7. lordblackfox              x9.125, wins: 0
+#8. azazel                    x8.680, wins: 0
+#9. mapostol                  x8.193, wins: 0
+#10. nikitin.alexandr         x5.648, wins: 0
+#11. sairi.na.tenshi          x4.684, wins: 0
+#12. kuchumovn                x4.544, wins: 0
+#13. juliasoft                x3.874, wins: 0
+#14. ibnrubaxa                x3.640, wins: 0
+#15. smitterz                 x2.922, wins: 0
+#16. rzcoder                  x2.296, wins: 0
+#17. an12345                  x1.865, wins: 0
+#18. karol_wegrzycki          x1.248, wins: 0
+#19. autarc                   x1.109, wins: 0
+#20. original                 x1.000, wins: 0
+#21. trialforlife             x0.656, wins: 0
+```
+
+
+---
+
+
+## Winners: without second argument (equivocal benchmark)
+```
+npm start --filter=arg
+-----------------------------------------------------------
+#1. ibnrubaxa                 x67.038, wins: 3
+#2. mapostol                  x50.130, wins: 0
+#3. kuchumovn                 x31.224, wins: 0
+#4. azazel                    x27.474, wins: 0
+#5. smitterz                  x27.443, wins: 0
+#6. dm.ashurov                x17.233, wins: 0
+#7. rzcoder                   x14.050, wins: 0
+#8. siranthony                x11.033, wins: 0
+#9. nikitin.alexandr          x9.372, wins: 0
+#10. lexich121                x8.579, wins: 0
+#11. newbiecraft              x6.657, wins: 0
+#12. juliasoft                x6.390, wins: 0
+#13. lordblackfox             x5.986, wins: 0
+#14. an12345                  x3.612, wins: 0
+#15. sergii.shpak             x3.552, wins: 0
+#16. sairi.na.tenshi          x3.022, wins: 0
+#17. karol_wegrzycki          x1.498, wins: 0
+#18. autarc                   x1.212, wins: 0
+#19. original                 x1.000, wins: 0
+#20. trialforlife             x0.749, wins: 0
+#21. original2                x0.610, wins: 0
+```
--- bench.js
+++ bench.js
@@ -1,52 +1,164 @@
 #!/usr/bin/node --harmony-generators
 var assert = require('assert');
-var suite = new (require('benchmark').Suite);
 
-var functions = [];
-var variants = ['original', 'siranthony', 'ibnrubaxa', 'rzcoder',
-    'newbiecraft', 'mapostol', 'smitterz', 'lordblackfox', 'an12345',
-    'lexich121', 'sergii.shpak', 'kuchumovn', 'sairi.na.tenshi', 'juliasoft',
-    'karol_wegrzycki', 'azazel', 'dm.ashurov', 'nikitin.alexandr',
-    'trialforlife', 'autarc'];
-variants.forEach(function(variant){
-    functions.push(require('./submissions/'+variant+'_strftime').strftime); });
+var users = ['original', 'original2', 'siranthony', 'ibnrubaxa', 'rzcoder',
+	'newbiecraft', 'mapostol', 'smitterz', 'lordblackfox', 'an12345',
+	'lexich121', 'sergii.shpak', 'kuchumovn', 'sairi.na.tenshi', 'juliasoft',
+	'karol_wegrzycki', 'azazel', 'dm.ashurov', 'nikitin.alexandr',
+	'trialforlife', 'autarc']
+;
 
-var test_cases = ['%A, %B %d, %Y', '%F %T', '%a, %d %b %Y %H:%M:%S'];
+var functions = users.map(function (variant) {
+	return require('./submissions/' + variant + '_strftime').strftime;
+});
+
+var filter = process.env.npm_config_filter;
+var formats = ['%A, %B %d, %Y', '%F %T', '%a, %d %b %Y %H:%M:%S'];
+var times = [];
 var dates = [];
 var count = 1000;
-var res, i;
-for (i = 0; i < count; i++)
-    dates.push(new Date((Math.random() + 0.5) * 1000000000000));
+var testCases = [];
+var testsCount = 0;
+var completed = 0;
+var statistics = {};
+var bar;
 
-test_cases.forEach(function(test_case){
-    console.log('--- sanity ('+test_case+') ---');
-    res = functions[0](test_case, dates[0]);
-    console.log('=== '+res);
-    for (i = 1; i < functions.length; i++)
-    {
-        console.log('* '+variants[i]);
-        assert.strictEqual(res, functions[i](test_case, dates[0]));
-    }
 
-    for (i = 0; i < functions.length; i++)
-    {
-        suite.add(variants[i]+' ('+test_case+')', function(fn){
-            return function(){
-                for (i = 0; i < count; i++)
-                    res = fn(test_case, dates[i]);
-            };
-        }(functions[i]))
-    }
-});
-
-suite.on('cycle', function(ev){ console.log(ev.target.toString()); })
-.on('complete', function(){
-    console.log('--- complete ---');
-    process.exit();
-});
-
-if (require.main===module)
-{
-    console.log('--- starting ---');
-    suite.run({async: false});
+// Testing data
+function resetData() {
+	for (var i = 0; i < count; i++) {
+		times.push((Math.random() + 0.5) * 1e13);
+		dates.push(new Date(times[i]));
+	}
 }
+
+
+function spaces(str, len) {
+	return str + new Array(len - str.length).join(' ');
+}
+
+
+function printWinners() {
+	for (var desc in statistics) {
+		var winners = statistics[desc], results = [];
+
+		for (var name in winners) {
+			var wins = winners[name];
+			results.push({
+				name: name,
+				x: wins.reduce(function (a, b) { return a + b.x }, 0) / wins.length,
+				wins: wins.reduce(function (a, b) { return a + b.win }, 0)
+			});
+		}
+
+		console.log('\nWinners: ' + desc);
+		console.log(new Array(60).join('-'));
+
+		results
+			.sort(function (a, b) { return b.x - a.x; })
+			.forEach(function (a, i) {
+				console.log(spaces('#' + (i + 1) + '. ' + a.name, 30) + ' x' + a.x.toFixed(3) + ', wins: ' + a.wins);
+			})
+		;
+	}
+}
+
+
+function test(desc, callback) {
+	if (filter && desc.indexOf(filter) === -1) {
+		return;
+	}
+
+	statistics[desc] = {};
+
+	testCases = testCases.concat(formats.map(function (format) {
+		return function () {
+			var suite = new (require('benchmark').Suite);
+
+			// Check
+			functions.forEach(function (fn, i) {
+				assert.strictEqual(
+					functions[0](format, dates[0]),
+					fn(format, dates[0]),
+					users[i]
+				);
+
+				testsCount++;
+				suite.add(users[i], function () {
+					callback(fn, format);
+				});
+			});
+
+
+			suite
+				.on('cycle', function (evt) {
+					bar.tick();
+//					console.log(evt.target.toString());
+				})
+				.on('complete', function () {
+					resetData();
+
+					var hz = this[0].hz;
+					var results = this.sort(function (a, b) {
+						a = a.stats;
+						b = b.stats;
+						return (a.mean + a.moe > b.mean + b.moe) ? 1 : -1;
+					});
+
+
+					results.forEach(function (result, i) {
+						(statistics[desc][result.name] = statistics[desc][result.name] || []).push({
+							x: result.hz / hz,
+							win: i == 0,
+							place: i + 1
+						});
+					});
+
+
+					if (testCases.length === ++completed) {
+						printWinners();
+						process.exit(0);
+					}
+				})
+			;
+
+			suite.run({ async: true });
+		};
+	}));
+}
+
+
+//
+// Tests
+//
+test('date', function (fn, format) {
+	for (var i = 0; i < count; i++) {
+		fn(format, dates[i]);
+	}
+});
+
+
+test('timestamp', function (fn, format) {
+	for (var i = 0; i < count; i++) {
+		fn(format, new Date(times[i]));
+	}
+});
+
+
+test('random', function (fn, format) {
+	fn(format, new Date((Math.random() + 0.5) * 1e13));
+});
+
+
+test('without second argument', function (fn, format) {
+	fn(format);
+});
+
+
+// Run
+resetData();
+testCases.forEach(function (run) {
+	run();
+});
+console.log('Running:', testsCount);
+bar = new (require('progress'))('Progress [:bar] :percent (:current/:total) :etas', { total: testsCount });
